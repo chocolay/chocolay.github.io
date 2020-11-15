b9395a921b3621d9e8a9920787838a2e6fe8ee9f
Elise <chocolay@users.noreply.github.com>
2020-11-14 20:11:46 -0500

    Update app.js

--- postapps/agent1/js/app.js
+++ postapps/agent1/js/app.js
@@ -398,7 +398,7 @@ d3.csv(url).then(function(data) {
     d3.select(id).append("svg")
         .attr("width",width)
         .attr("height",height);
-console.log(d3.select("svg").size())
+    console.log(d3.select("svg").size())
 
     obj = { "params": {"currentDate":"14/09/2020"},
         "adjustableParams":[
@@ -551,221 +551,221 @@ console.log(d3.select("svg").size())
             start(d);
         });//sortButton click
 
-                var svg = d3.select("svg")
-                    .attr("width",width)
-                    .attr("height",height)
-                    .attr("id", "mainSVG")
-                    .append("g")
-                    .classed("forecast",1);
+        var svg = d3.select("svg")
+            .attr("width",width)
+            .attr("height",height)
+            .attr("id", "mainSVG")
+            .append("g")
+            .classed("forecast",1);
 
-                svg.append("rect")
-                    .attr("width", width)
-                    .attr("height", height)
-                    .attr("id", "backgroundRect");
+        svg.append("rect")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("id", "backgroundRect");
 
-                svg.append("text")
-                    .attr("id","timestamp")
-                    .style("fill","white")
-                    .attr("transform","translate(10,20)");
+        svg.append("text")
+            .attr("id","timestamp")
+            .style("fill","white")
+            .attr("transform","translate(10,20)");
 
-                svg.selectAll(".unit").data(data).enter()
-                    .append("g")
-                    .attr("class", "unit")
-                    .attr("transform","translate(480,300)")
-                    .append("circle")
-                    .attr("r",function(d) {return radiusScale(d["Square Feet"])})
-                clearFilters(obj);
-            }//initVis
+        svg.selectAll(".unit").data(data).enter()
+            .append("g")
+            .attr("class", "unit")
+            .attr("transform","translate(480,300)")
+            .append("circle")
+            .attr("r",function(d) {return radiusScale(d["Square Feet"])})
+        clearFilters(obj);
+    }//initVis
 
 
-            function setup(obj, data) {
-                obj.data = data;
-                obj.formats = {
-                    timeParse: d3.timeParse("%d/%m/%Y"),
-                    timeFormat: d3.timeFormat("%d/%m/%Y"),
-                    timestampFormat:d3.timeFormat("%b %d, %Y")
-                };
-                obj.data.forEach(function(unit,index) {
-                    timeKeys = ["Report Date", "Lease End Date Current Lease", "Lease End Date Future Lease", "Lease Start Date Future Lease", "Make Ready Date Future Lease", "Move In Date Future Lease", "Notice For Date Current Lease", "Notice On Date Current Lease"];
-                    timeKeys.forEach(function(key) {
-                        unit[key] = obj.formats.timeParse(unit[key])
-                        if (!unit[key] || unit[key] == "") unit[key] = obj.formats.timeParse(obj.params.currentDate);
-                    });
-                    unit.id = "id"+index;
-                    unit.All = 1;
-                    unit["Size"] = +unit["Size"];
+    function setup(obj, data) {
+        obj.data = data;
+        obj.formats = {
+            timeParse: d3.timeParse("%d/%m/%Y"),
+            timeFormat: d3.timeFormat("%d/%m/%Y"),
+            timestampFormat:d3.timeFormat("%b %d, %Y")
+        };
+        obj.data.forEach(function(unit,index) {
+            timeKeys = ["Report Date", "Lease End Date Current Lease", "Lease End Date Future Lease", "Lease Start Date Future Lease", "Make Ready Date Future Lease", "Move In Date Future Lease", "Notice For Date Current Lease", "Notice On Date Current Lease"];
+            timeKeys.forEach(function(key) {
+                unit[key] = obj.formats.timeParse(unit[key])
+                if (!unit[key] || unit[key] == "") unit[key] = obj.formats.timeParse(obj.params.currentDate);
+            });
+            unit.id = "id"+index;
+            unit.All = 1;
+            unit["Size"] = +unit["Size"];
+        });
+        obj.crossfilter = crossfilter(data);
+
+        obj.crossfilter.dims = {};
+
+        var dims = ["id","Status", "Lease End", "Lease Start Date Future Lease", "Renewal Status", "Make Ready", "Lease End Date Current Lease","Notice For Date Current Lease"] //TY: added "Notice For Date Current Lease"
+            .forEach(function(key) {
+                obj.crossfilter.dims[key] = obj.crossfilter.dimension(function(d) {
+                    return d[key]
                 });
-                obj.crossfilter = crossfilter(data);
+            });
 
-                obj.crossfilter.dims = {};
+        obj.numUnits = obj.data.length;
+        obj.data = d3.shuffle(obj.data);
+        return obj
+    } //setup
 
-                var dims = ["id","Status", "Lease End", "Lease Start Date Future Lease", "Renewal Status", "Make Ready", "Lease End Date Current Lease","Notice For Date Current Lease"] //TY: added "Notice For Date Current Lease"
-                    .forEach(function(key) {
-                        obj.crossfilter.dims[key] = obj.crossfilter.dimension(function(d) {
-                            return d[key]
-                        });
-                    });
+    //start up a simulation & visualization of it
+    start = function(key) {
+        d3.select("#go").datum("go").text("stop"); //the datum indicates the simulation is in "go" mode.
+        //the text on the button tells the user to click on it to stop the motion
 
-                obj.numUnits = obj.data.length;
-                obj.data = d3.shuffle(obj.data);
-                return obj
-            } //setup
+        duration = 1000-(+d3.select("#durationSlider").property("value"));
 
-            //start up a simulation & visualization of it
-            start = function(key) {
-                d3.select("#go").datum("go").text("stop"); //the datum indicates the simulation is in "go" mode.
-                //the text on the button tells the user to click on it to stop the motion
+        d3.select("#toggle").datum(key); //store the field we are going to sort on
 
-                duration = 1000-(+d3.select("#durationSlider").property("value"));
+        //set up a new forcing function for the visualization
+        visualSimulation = d3.forceSimulation(obj.data)
+            .force("y", d3.forceY(70))
+            .force("charge", d3.forceManyBody().strength(-6))
+            .alphaTarget(0.3)
+            .on("tick", visTick);
+        createCenters(visualSimulation,key);
 
-                d3.select("#toggle").datum(key); //store the field we are going to sort on
-
-                //set up a new forcing function for the visualization
-                visualSimulation = d3.forceSimulation(obj.data)
-                    .force("y", d3.forceY(70))
-                    .force("charge", d3.forceManyBody().strength(-6))
-                    .alphaTarget(0.3)
-                    .on("tick", visTick);
-                createCenters(visualSimulation,key);
-
-                timer = d3.interval(function(tim) {
-                    if (obj.currentDate>new Date(2030,0,1)) {
-                        timer.stop();
-                        visualSimulation.stop();
-                    }
-                    let ttime = obj.formats.timestampFormat(obj.currentDate);
-                    d3.select("#timestamp").text(ttime);
-                    runSimulation(obj.data,obj);
-                    createCenters(visualSimulation,key);
-                },duration);
-
-                d3.selectAll("path.line").each(function() {
-                    variable = d3.select(this.parentElement.parentElement).attr("id");
-                    d3.select(this).transition()
-                        .duration(duration)
-                        .ease(d3.easeLinear)
-                        .on("start", plotTick(variable));
-                });
-            }//start
-
-            stopForecast = function() {
-                d3.select("#go").datum("stop").text("go"); //the datum indicates the simulation is in "go" mode.
-                //the text on the button tells the user to click on it to stop the motion
+        timer = d3.interval(function(tim) {
+            if (obj.currentDate>new Date(2030,0,1)) {
                 timer.stop();
                 visualSimulation.stop();
-            } //stop
+            }
+            let ttime = obj.formats.timestampFormat(obj.currentDate);
+            d3.select("#timestamp").text(ttime);
+            runSimulation(obj.data,obj);
+            createCenters(visualSimulation,key);
+        },duration);
+
+        d3.selectAll("path.line").each(function() {
+            variable = d3.select(this.parentElement.parentElement).attr("id");
+            d3.select(this).transition()
+                .duration(duration)
+                .ease(d3.easeLinear)
+                .on("start", plotTick(variable));
+        });
+    }//start
+
+    stopForecast = function() {
+        d3.select("#go").datum("stop").text("go"); //the datum indicates the simulation is in "go" mode.
+        //the text on the button tells the user to click on it to stop the motion
+        timer.stop();
+        visualSimulation.stop();
+    } //stop
 
 //move the circles
-            function visTick() {
-                increment+=1;
-                d3.selectAll("circle")
-                    .attr("transform",function(d) {
-                        return "translate("+ d.x+","+ d.y+")"
-                    })
-                    .style("fill",function(d) {
-                        return color(d["Status"])
-                    })
-                    .style("stroke",function(d) {
-                        d["Status"]=="VA"?"white":"none"
-                    })
-                    .style("stroke-width",function(d) {
-                        d["Status"]=="VA"?2:0
-                    })
-                //playing with some ways to highlight the motion
-                /*
-                 .attr("r",function(d) {
-                 var radius = xScale(d["Square Feet"]);
-                 if (increment>100) {
-                 radius *= Math.min(1.5,Math.max(1,Math.abs(d.vx)));
-                 }
-                 return radius
-                 });
-                 d3.selectAll("circle")
-                 .style("opacity",function(d) {
-                 return Math.abs(d.vx)>1?1:0.9
-                 })
-                 */
-            } //visTick
+    function visTick() {
+        increment+=1;
+        d3.selectAll("circle")
+            .attr("transform",function(d) {
+                return "translate("+ d.x+","+ d.y+")"
+            })
+            .style("fill",function(d) {
+                return color(d["Status"])
+            })
+            .style("stroke",function(d) {
+                d["Status"]=="VA"?"white":"none"
+            })
+            .style("stroke-width",function(d) {
+                d["Status"]=="VA"?2:0
+            })
+        //playing with some ways to highlight the motion
+        /*
+         .attr("r",function(d) {
+         var radius = xScale(d["Square Feet"]);
+         if (increment>100) {
+         radius *= Math.min(1.5,Math.max(1,Math.abs(d.vx)));
+         }
+         return radius
+         });
+         d3.selectAll("circle")
+         .style("opacity",function(d) {
+         return Math.abs(d.vx)>1?1:0.9
+         })
+         */
+    } //visTick
 
-            //set up the centers based on the number of categoryCounts
-            function createCenters(vs,key) {
-                var Ncenters = categoryCounts[key];
-                var centerPos_x = [];
-                var catWidth = (width-100)/(Ncenters+1);
-                if (Ncenters==1) centerPos_x = [0];
-                else {
-                    d3.range(0,Ncenters).forEach(function(i) {
-                        centerPos_x.push(catWidth*i-325);
-                    });
-                }
-                centerPos_x.forEach(function(d,i) {
-                    vs.force("center_x_"+i+"_"+Ncenters,isolate(d3.forceX(centerPos_x[i]),
-                        function(d) {
-                            j = codes[key][i];
-                            return d[key] === j; },obj))
-                });
-                d3.selectAll("text.categoryLabel").remove();
-                if (key!="All") {
-                    d3.select("#mainSVG .forecast").selectAll("text.categoryLabel")
-                        .data(codes[key]).enter()
-                        .append("text")
-                        .classed("categoryLabel",1)
-                        .text(function(d) {return d})
-                        .style("fill","white")
-                        .attr("text-anchor","left")
-                        .attr("transform",function(d,i) {
-                            return "translate("+(centerPos_x[i]*1.2+450)+",225)"
-                        });
-                }
-
-                return centerPos_x;
-            } //createCenters
-
-
-            function makeSlider(sel, obj) {
-                var humanName = obj.name;
-                var variable = obj.variable;
-                var defaultValue = obj.value;
-                var domain = obj.domain;
-                var sel2 = sel.append("div")
-                    .classed("inputBox", 1)
-                    .attr("id", variable);
-
-
-
-                sel2.append("input")
-                    .property("type", "range")
-                    .property("min", domain[0])
-                    .property("max", domain[1])
-                    .property("step", domain[2])
-                    .property("value", defaultValue)
-                    .classed("slider", 1)
-                    .style("margin-left","20px")
-                    .on("input", change);
-
-                val = sel2.select("input").property("value")
-                sel2.append("div")
-                    .datum(humanName)
-                    .text(humanName + ": "+val)
-                    .classed("label", 1)
-                    .style("margin-left","20px")
-                    .style("color","#3e3e45");
-
-
-                function change() {
-                    //update everything about the slider
-                    sel2.selectAll("input.slider)
-                        .property("value", this.value);
-                    sel2.select(".label")
-                        .text(d=>d + ": "+val)
-                    obj.value = this.value;
-                }
-     } //makeSlider
+    //set up the centers based on the number of categoryCounts
+    function createCenters(vs,key) {
+        var Ncenters = categoryCounts[key];
+        var centerPos_x = [];
+        var catWidth = (width-100)/(Ncenters+1);
+        if (Ncenters==1) centerPos_x = [0];
+        else {
+            d3.range(0,Ncenters).forEach(function(i) {
+                centerPos_x.push(catWidth*i-325);
+            });
+        }
+        centerPos_x.forEach(function(d,i) {
+            vs.force("center_x_"+i+"_"+Ncenters,isolate(d3.forceX(centerPos_x[i]),
+                function(d) {
+                    j = codes[key][i];
+                    return d[key] === j; },obj))
         });
+        d3.selectAll("text.categoryLabel").remove();
+        if (key!="All") {
+            d3.select("#mainSVG .forecast").selectAll("text.categoryLabel")
+                .data(codes[key]).enter()
+                .append("text")
+                .classed("categoryLabel",1)
+                .text(function(d) {return d})
+                .style("fill","white")
+                .attr("text-anchor","left")
+                .attr("transform",function(d,i) {
+                    return "translate("+(centerPos_x[i]*1.2+450)+",225)"
+                });
+        }
 
-        d3.timeout(function() {
-            var key = "Status";
-            var increment = 0;
-            start(key);
-        },500);
+        return centerPos_x;
+    } //createCenters
+
+
+    function makeSlider(sel, obj) {
+        var humanName = obj.name;
+        var variable = obj.variable;
+        var defaultValue = obj.value;
+        var domain = obj.domain;
+        var sel2 = sel.append("div")
+            .classed("inputBox", 1)
+            .attr("id", variable);
+
+
+
+        sel2.append("input")
+            .property("type", "range")
+            .property("min", domain[0])
+            .property("max", domain[1])
+            .property("step", domain[2])
+            .property("value", defaultValue)
+            .classed("slider", 1)
+            .style("margin-left","20px")
+            .on("input", change);
+
+        val = sel2.select("input").property("value")
+        sel2.append("div")
+            .datum(humanName)
+            .text(humanName + ": "+val)
+            .classed("label", 1)
+            .style("margin-left","20px")
+            .style("color","#3e3e45");
+
+
+        function change() {
+            //update everything about the slider
+            sel2.selectAll("input.slider")
+                .property("value", this.value);
+            sel2.select(".label")
+                .text(d=>d + ": "+val)
+            obj.value = this.value;
+        }
+    } //makeSlider
+});
+
+d3.timeout(function() {
+    var key = "Status";
+    var increment = 0;
+    start(key);
+},500);
 
