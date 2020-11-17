f35c5af0c5cb68123aabf4dcaba0eeb41f1e5f69
Elise <chocolay@users.noreply.github.com>
2020-11-16 21:56:40 -0500

    Update d3_app.js

--- postapps/link2trees/js/d3_app.js
+++ postapps/link2trees/js/d3_app.js
@@ -1039,5 +1039,8 @@ input2name = "/postapps/link2trees/data/input3.json",
 
 }();
 
-d3.select("#VMdisplay").on("click",function() {console.log(this)})
+d3.select("#VMdisplay, #VMdisplay *").on("click",function() {
+	var toggle = d3.select("#VMdisplay ul").style(display")=="none"
+	d3.select("#VMdisplay *").style("display",toggle?"inline-block":"none")
+})
 
