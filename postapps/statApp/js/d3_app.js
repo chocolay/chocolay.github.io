a1de7a313dba3047a78b5b0b52368da208ac0d1a
Elise <chocolay@users.noreply.github.com>
2020-11-16 19:51:42 -0500

    Update d3_app.js

--- postapps/statApp/js/d3_app.js
+++ postapps/statApp/js/d3_app.js
@@ -1875,13 +1875,13 @@ eqn.append("text")
 	.attr("transform","translate(202,-30)")
 	.text('x')	
 
-d3.select("#numerator").append("tspan").attr("baseline-shift","super").text('&alpha;-1')
-d3.select("#numerator").append("tspan").text('(1-x)')
-d3.select("#numerator").append("tspan").attr("baseline-shift","super").text('&beta;-1')     
+d3.select("#numerator").append("tspan").attr("baseline-shift","super").html('&alpha;-1')
+d3.select("#numerator").append("tspan").html('(1-x)')
+d3.select("#numerator").append("tspan").attr("baseline-shift","super").html('&beta;-1')     
 
  eqn.append("text")
 	.attr("transform","translate(140,30)")
-	.text( '(&alpha;-1)! (&beta;-1)! / (&alpha;+&beta;-1)!');
+	.html( '(&alpha;-1)! (&beta;-1)! / (&alpha;+&beta;-1)!');
 
 
 eqn.append("line")
