cbce257013ef12a6cae62ac400965bf59a94618f
Elise <chocolay@users.noreply.github.com>
2020-11-18 17:34:18 -0500

    Create praise.js

---
+++ postapps/praise/js/praise.js
@@ -0,0 +1,14 @@
+let words = [];
+
+d3.selectAll("text").each(function(d,i) { 
+let T = d3.select(this);
+let P = d3.select(this.parentElement);
+words[i] = {
+  word: T.text(),
+  color: T.style("fill"),
+  size: T.style("font-size"),
+  x: +T.attr("x"),
+  y: +T.attr("y"),
+  tranform: P.style("transform")
+  };
+});
