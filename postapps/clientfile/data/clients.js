62ce8769e25678f7a388b1cb76fbac90dbd37468
Elise <chocolay@users.noreply.github.com>
2020-11-17 19:42:53 -0500

    Create clients.js

---
+++ postapps/clientfile/data/clients.js
@@ -0,0 +1,34 @@
+city,countrycode,latitude,longitude
+Sydney,AU,-22.869,151
+Melbourne,AU,-37.814,145
+Cairns,AU,-16.918,146
+Sofia,BG,42.6875,23.325
+Montreal,CA,45.509,-73.588
+Geneva,CH,46.2022,6.145
+London,GB,51.508,-0.12574
+Penzance,GB,50.118,-5.527
+Birmingham,GB,52.4814,-1.8998
+Southampton,GB,50.904,-1.404
+Dublin,IE,53.33,-6.25
+Rome,IT,41.892,12.511
+KL,MY,3.14,101.7
+Oslo,NO,59.912,10.746
+Moscow,RU,55.752,37.616
+Novosibirsk,RU,55.0415,82.935
+Bangkok,TH,13.75,100.501
+Palo Alto,US,37.445,-122.1603
+Orange County,US,33.79,-117.85
+Palo Alto,US,37.445,-122.1603
+Boston,US,45.21,-71
+Atlanta,US,33.749,-84.388
+Burlington,US,44.476,-73.212
+Milwaukee,US,43.0389,-87.907
+Lawrence Kansas,US,38.97167,-95.23515
+Minneapolis,US,44.98,-93.264
+San Diego,US,32.7153,-117.157
+Salt Lake City,US,40.761,-111.891
+Washington DC,US,38.895,-77.036
+Big Island,US,19.73,-155.1
+Huntsville,US,34.73,-86.58
+Seattle,US,47.606,-122.332
+Nashville,US,36.166,-87.78
