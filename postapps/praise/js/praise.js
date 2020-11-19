a647777cbd8984a2d32ffca12385cc9b9d958469
Elise <chocolay@users.noreply.github.com>
2020-11-18 19:42:34 -0500

    Update praise.js

--- postapps/praise/js/praise.js
+++ postapps/praise/js/praise.js
@@ -1,10 +1,8 @@
-
-d3.select("svg").style("background","#322");
-
-d3.select("#svg1").style("top",-parseInt(d3.select("#svg2").style("height"))+"px")
+let h = parseInt(d3.select("#svg2").style("height"));
+d3.select("#svg1")
+  .style("top",(-h)+"px")
              
 let words = [];
-
 d3.selectAll("text").each(function(d,i) { 
 let T = d3.select(this);
 let P = d3.select(this.parentElement);
