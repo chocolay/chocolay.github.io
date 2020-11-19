4d66283697b1b76f91a306ece656d612230fb104
Elise <chocolay@users.noreply.github.com>
2020-11-19 09:37:11 -0500

    Update d3_app.js

--- postapps/topics/d3_app.js
+++ postapps/topics/d3_app.js
@@ -6,7 +6,7 @@ var s = 320,
     paper = d3.select('#d3_app').append('svg').attr({
         "width": s,
         "height": s/2,
-        "viewbox":"-480 -240 480 240"
+        "viewbox":"-1000 -1000 5000 500"
     }).append("g"),
     tau = 2 * Math.PI, period = tau * 4 * Math.pow(radius, 2) / gamma, dt = period / 100,
     comega = Math.cos(dt / (2 * period)), somega = Math.sin(dt / (2 * period)),
@@ -45,10 +45,8 @@ function animate() {
     it++;
     advance();
     resample();
-    patchbd.attr({
-        'd': line(p),
-        "transform": "rotate(" + (it * angstep)+")"
-    })
+    patchbd.attr('d', line(p))
+    //.attr("transform","rotate("+(it*angstep)+")")
     return it > Nt
 }
 
