d8512894df99819104abbc4f2a2f62979b90edcf
Elise <chocolay@users.noreply.github.com>
2020-11-18 19:55:13 -0500

    Update praise.js

--- postapps/praise/js/praise.js
+++ postapps/praise/js/praise.js
@@ -1,7 +1,14 @@
+let header = d3.select("h1").text();
+d3.select(".page-title-box").remove();
+
+d3.select(".content .container")
+  .style("text-align","center")
+  .append("h1").text(header);
+
 let h = parseInt(d3.select("#svg2").style("height"));
 d3.select("#svg1")
-  .style("top",(-h)+"px")
-             
+  .style("top",(-h)+"px");
+   
 let words = [];
 d3.selectAll("text").each(function(d,i) { 
 let T = d3.select(this);
