115df3aed155d51f17416ebf15004109fbb15054
Elise <chocolay@users.noreply.github.com>
2020-11-20 01:37:07 -0500

    Update d3_app.js

--- postapps/topics/d3_app.js
+++ postapps/topics/d3_app.js
@@ -6,7 +6,7 @@ var s = 960, S = 500, ss = "100%"
     paper = d3.select('#d3_app').append('svg').attr({
         "width": ss,
         "height": ss
-    }).attr("viewbox","0 0+" "+s+" "+ (s/2))
+    }).attr("viewbox","0 0 "+s+" "+ (s/2))
             .style("border","5px solid red")
     .append("g")
 .attr("transform","translate(0,40)");
